08f3f828eb5e90aa40ca13df3dbeca7897a833f4
dan <ldr200@mail.ru>
2024-02-02 19:06:00 +0300

    fix grid game-field and add background

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+// document.getElementById("run_game_image").
+function start_game(start_button) {
+    start_button.parentNode.parentNode.remove();
+    let washer = document.createElement("img", {"src": "Assets/sprites/шайба.png"});
+    console.log("dsdsdsdsdsdsd")
+
+}
